628803c2b240eaf7d2394f7a0d5f46bce0b4a868
phillip rand <n>
2022-04-13 17:56:38 +0000

    updated config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Tok9ps7k9', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7iv90roqb774t8pof8is7v41pp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_omznbAUJp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5dgurpuefac14qfgrrfh6iaggr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vfnp61iso1.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''//'https://vfnp61iso1.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
